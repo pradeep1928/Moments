bcb743a5204683030ff486da0285851921fa8dbb
Pradeep Padmukhi <ppadmukhi28@gmail.com>
2022-04-16 13:49:45 +0530

    api interceptors added for authentication

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,15 @@
 import axios from 'axios';
 
 // backend url for connection
-const API = axios.create({ baseURL: 'http://localhost:5000' })
+const API = axios.create({ baseURL: 'http://localhost:5000' });
+
+API.interceptors.request.use((req) => {
+    if (localStorage.getItem('profile')) {
+        req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
+    }
+
+    return req;
+})
 
 // Here using url we are doing actions on backend data.
 export const fetchPosts = () => API.get('/posts');
