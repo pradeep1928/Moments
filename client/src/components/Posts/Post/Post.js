b655f90fc0c0c80fe700778968dfa3854260bc28
Pradeep Padmukhi <ppadmukhi28@gmail.com>
2022-04-17 00:20:51 +0530

    like and dislike functionality added in client side

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -9,17 +9,50 @@ import {
   Typography,
 } from "@material-ui/core/";
 import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
+import ThumbUpAltOutlined from "@material-ui/icons/ThumbUpAltOutlined";
 import DeleteIcon from "@material-ui/icons/Delete";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 import moment from "moment";
 
-// local file imports 
+// local file imports
 import { deletePost, likePost } from "../../../actions/posts";
 import useStyles from "./styles";
 
 const Post = ({ post, setCurrentId }) => {
   const classes = useStyles();
   const dispatch = useDispatch();
+  const user = JSON.parse(localStorage.getItem("profile"));
+ 
+  // function to show like or likes for you and others 
+  const Likes = () => {
+    if (post.likes.length > 0) {
+      return post.likes.find(
+        (like) => like === (user?.result?.googleId || user?.result?._id)
+      ) ? (
+        <>
+          <ThumbUpAltIcon fontSize="small" />
+          &nbsp;
+          {post.likes.length > 2
+            ? `You and ${post.likes.length - 1} others`
+            : `${post.likes.length} like${
+                post.likes.length > 1 ? "s" : ""
+              }`}
+        </>
+      ) : (
+        <>
+          <ThumbUpAltOutlined fontSize="small" />
+          &nbsp; {post.likes.length}{" "}
+          {post.likes.length === 1 ? "like" : "likes"}{" "}
+        </>
+      );
+    }
+    return (
+      <>
+        <ThumbUpAltOutlined fontSize="small" /> &nbsp;Like
+      </>
+    );
+  };
+
 
   return (
     <Card className={classes.card}>
@@ -32,8 +65,8 @@ const Post = ({ post, setCurrentId }) => {
       <div className={classes.overlay}>
         <Typography variant="h6">{post.name}</Typography>
         <Typography variant="body2">
-          {moment(post.createdAt).fromNow()}  {/*this will show the time duretion when post is created from now*/}
-
+          {moment(post.createdAt).fromNow()}{" "}
+          {/*this will show the time duretion when post is created from now*/}
         </Typography>
       </div>
 
@@ -77,12 +110,14 @@ const Post = ({ post, setCurrentId }) => {
         <Button
           size="small"
           color="primary"
+          disabled={!user?.result}
           onClick={() => dispatch(likePost(post._id))}
         >
-          <ThumbUpAltIcon fontSize="small" /> &nbsp; Like &nbsp; {post.likeCount}{" "}
+         <Likes />
+          {post.likeCount}{" "}
         </Button>
 
-      {/* delete post button*/}
+        {/* delete post button*/}
         <Button
           size="small"
           color="primary"
